a6bf305d38399684d1ef6e16e09ba3aac4d076a2
Andy <70196193+andychuah@users.noreply.github.com>
2023-08-15 23:30:39 +0800

    Add files via upload

---
+++ Leetcode-86.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        sList, bList = ListNode(), ListNode()
+        small, big = sList, bList
+
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+
+        small.next = bList.next
+        big.next = None
+
+        return sList.next
