b388e8471854b09b215d5ff58af652c1473f3d7a
Andy <70196193+andychuah@users.noreply.github.com>
2023-10-16 19:03:22 +0800

    Add files via upload

---
+++ Leetcode-119.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = [1]
+        prev = 1
+        for i in range(1, rowIndex + 1):
+            next_val = prev * (rowIndex - i + 1) / i
+            res.append(next_val)
+            prev = next_val
+        return res
