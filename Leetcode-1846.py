2a7aa548810a2ad42d67c72e490d2c9cacaca729
Andy <70196193+andychuah@users.noreply.github.com>
2023-11-15 20:26:12 +0800

    Add files via upload

---
+++ Leetcode-1846.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        arr[0] = 1
+
+        for i in range(1, len(arr)):
+            if abs(arr[i] - arr[i-1]) <= 1:
+                continue
+            else:
+                arr[i] = arr[i-1] + 1
+        return arr[-1]
+        
