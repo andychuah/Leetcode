f481f1b603d9257cf7589e5e9e36e5ab8748363b
Andy <70196193+andychuah@users.noreply.github.com>
2023-11-18 00:00:59 +0800

    Add files via upload

---
+++ Leetcode-1877.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        min_max_pair_sum = float('-inf')
+
+        while left < right:
+            current_pair_sum = nums[left] + nums[right]
+            min_max_pair_sum = max(min_max_pair_sum, current_pair_sum)
+            left += 1
+            right -= 1
+
+        return min_max_pair_sum
