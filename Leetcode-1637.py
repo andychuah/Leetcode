8c96893ebb24d7fa99ae1a8437e0fe60303a5ec9
Andy <70196193+andychuah@users.noreply.github.com>
2023-12-21 20:54:27 +0800

    Add files via upload

---
+++ Leetcode-1637.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxWidthOfVerticalArea(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort()
+        max_width = 0
+
+        for i in range(1, len(points)):
+            width = points[i][0] - points[i-1][0]
+            max_width = max(max_width, width)
+
+        return max_width
