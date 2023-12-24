d252239ab1c897ed1c2f2c2caaa08f2e7fa7b0d0
Andy <70196193+andychuah@users.noreply.github.com>
2023-12-24 14:10:10 +0800

    Add files via upload

---
+++ Leetcode-1758.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minOperations(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        count = 0
+        if n == 1:
+            return 0
+        for i in range(n):
+            if i % 2 == 0 and s[i] == '1':
+                count += 1
+            if i % 2 == 1 and s[i] == '0':
+                count += 1
+        return min(count, n - count)
+        
